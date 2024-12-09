1a4b31316193fcc20d48ef223879c39e195c0398
aadibhat09 <aadibhat09@gmail.com>
2024-12-09 09:40:25 -0800

    new wallpaper + blur effect on homepage

---
+++ assets/js/blurBackground.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.style.backgroundImage = 'url("assets/images/png/wallpaper-3.png")';
+    setTimeout(() => {
+        document.body.style.backgroundImage = 'url("assets/images/png/wallpaper-4.png")';
+    }, 800);
+});
