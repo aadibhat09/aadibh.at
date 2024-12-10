8e7c613a032ddcd5169ee5d47f06f66e6b8e4bd6
aadibhat09 <aadibhat09@gmail.com>
2024-12-09 18:32:23 -0800

    commented out blur background stuff

--- assets/js/blurBackground.js
+++ assets/js/blurBackground.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.body.style.backgroundImage = 'url("assets/images/png/wallpaper-3.png")';
-    setTimeout(() => {
-        document.body.style.backgroundImage = 'url("assets/images/png/wallpaper-4.png")';
-    }, 800);
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.body.style.backgroundImage = 'url("assets/images/png/wallpaper-3.png")';
+//     setTimeout(() => {
+//         document.body.style.backgroundImage = 'url("assets/images/png/wallpaper-4.png")';
+//     }, 800);
+// });
